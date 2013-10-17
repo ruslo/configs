c87deb1ae2c573377292d15e66c39f25b520250b
Ruslan Baratov <ruslan_baratov@yahoo.com>
2013-10-17 17:47:50 +0400

    Add gvim.py: gvim/vim wrapper/switcher script

---
+++ python/detail/command.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2013, Ruslan Baratov
+# All rights reserved.
+
+import subprocess
+import sys
+
+import detail.os_detect
+import detail.utility
+
+def print_install_memo_and_exit(cmd):
+  print(cmd, 'command not found, please install it')
+  if detail.os_detect.windows:
+    pass
+  elif detail.os_detect.cygwin:
+    pass
+  elif detail.os_detect.ubuntu: # check it before linux
+    print("You can try 'sudo apt-get install {}'".format(cmd))
+  elif detail.os_detect.gentoo: # check it before linux
+    print("You can try 'emerge -v {}'".format(cmd))
+  elif detail.os_detect.linux:
+    pass
+  elif detail.os_detect.macosx:
+    print("You can try 'sudo port install {}'".format(cmd))
+  else:
+    detail.utility.unreachable()
+  sys.exit("Error")
+
+def test_exists(cmd):
+  """Run |which| and if |cmd| not found return |False|, otherwise |True|"""
+  if detail.os_detect.windows:
+    which = 'where'
+  else:
+    which = 'which'
+  try:
+    subprocess.check_output([which, which])
+  except subprocess.CalledProcessError:
+    sys.exit("Internal error: '{}' not found".format(which))
+  try:
+    subprocess.check_output([which, cmd])
+    return True
+  except subprocess.CalledProcessError:
+    return False
+
+# TODO, 3.3 version use shutil.which
+def check_exist(cmd):
+  """Run |which| and if |cmd| not found print help installation text and exit"""
+  if not test_exists(cmd):
+    print_install_memo_and_exit(cmd)
+
+def run(cmd):
+  """Run given |cmd|, check exit OK and return list of output lines"""
+  command_name = cmd[0]
+  check_exist(command_name)
+  try:
+    output = subprocess.check_output(cmd, universal_newlines=True)
+    return output.split('\n')[:-1] # remove last
+  except subprocess.CalledProcessError as exc:
+    print('Failed to run "{}"'.format(' '.join(cmd)))
+    raise exc
---
+++ python/gvim.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2013, Ruslan Baratov
+# All rights reserved.
+
+# This is a wrapper/switcher to gvim/vim editor
+
+import os
+import subprocess
+import sys
+
+import detail.os_detect
+import detail.command
+
+vim_argv = sys.argv
+
+class Log:
+  def __init__(self, verbose):
+    self.verbose = verbose
+
+  def p(self, message):
+    if not self.verbose:
+      return
+    print(message)
+
+print_verbose = '--verbose' in vim_argv
+log = Log(print_verbose)
+if print_verbose:
+  vim_argv.remove('--verbose')
+
+def mac_vim_app_directory():
+  """Try to detect MacVim.app location"""
+  vim_app_dir = os.getenv('VIM_APP_DIR')
+  if vim_app_dir:
+    log.p('VIM_APP_DIR detected')
+    macvim_app_dir = os.path.join(vim_app_dir, 'MacVim.app')
+    if os.path.exists(macvim_app_dir):
+      return macvim_app_dir
+    sys.exit("Error: VIM_APP_DIR variable don't point to MacVim.app directory")
+  search_paths = [
+      '~/Applications',
+      '~/Applications/vim',
+      '/Applications',
+      '/Applications/vim',
+      '/Applications/Utilities',
+      '/Applications/Utilities/vim'
+  ]
+  for location in search_paths:
+    macvim_app_dir = os.path.join(location, 'MacVim.app')
+    macvim_app_dir = os.path.expanduser(macvim_app_dir)
+    log.p('test location: {}'.format(macvim_app_dir))
+    if os.path.exists(macvim_app_dir):
+      return macvim_app_dir
+  print("Used environment variable: VIM_APP_DIR")
+  print("Used standard paths: {}".format(search_paths))
+  sys.exit("Error: can't find MacVim.app directory")
+
+def vim_macosx_binary():
+  """Try to detect MacVim.app directory with Vim binary"""
+  dir = mac_vim_app_directory()
+  bin = os.path.join(dir, 'Contents', 'MacOS', 'Vim')
+  if os.path.exists(bin):
+    return bin
+  sys.exit("Error: path '{}' not exists".format(bin))
+
+def vim_binary():
+  if detail.os_detect.macosx:
+    return vim_macosx_binary()
+  else:
+    return 'gvim'
+
+vim_argv[0] = vim_binary()
+
+if detail.command.test_exists(vim_argv[0]):
+  if detail.os_detect.macosx:
+    vim_argv.insert(1, '-g')
+else:
+  # gvim not found, downgrade to vim
+  vim_argv[0] = 'vim'
+  detail.check_exists(vim_argv[0])
+
+log.p('call: {}'.format(vim_argv))
+
+# do not call |command.run| (tested with '-f' option)
+subprocess.check_call(vim_argv)
