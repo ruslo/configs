771ff4a96701877be6709145dc3c7f212ec4c4c3
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-12-24 17:40:09 +0300

    cmd.py fix

--- python/cmd.py
+++ python/cmd.py
@@ -26,17 +26,19 @@ cwd_winpath = detail.os.cygwin_to_win(os.getcwd())
 tmp_path = '/tmp/configs.python.cmd.py-temp.bat'
 tmp_winpath = detail.os.cygwin_to_win(tmp_path)
 
-if not os.path.exists(tmp_path):
-  temp = open(tmp_path, 'w')
-  temp.write('{}:\n'.format(cwd_winpath[0]))
-  temp.write('cd "{}"\n'.format(cwd_winpath))
-  temp.write('cmd\n')
+if os.path.exists(tmp_path):
+  os.remove(tmp_path)
 
-  os.chmod(
-      tmp_path,
-      stat.S_IXOTH | stat.S_IXGRP | stat.S_IXUSR |
-      stat.S_IROTH | stat.S_IRGRP | stat.S_IRUSR
-  )
+temp = open(tmp_path, 'w')
+temp.write('{}:\n'.format(cwd_winpath[0]))
+temp.write('cd "{}"\n'.format(cwd_winpath))
+temp.write('cmd\n')
+
+os.chmod(
+    tmp_path,
+    stat.S_IXOTH | stat.S_IXGRP | stat.S_IXUSR |
+    stat.S_IROTH | stat.S_IRGRP | stat.S_IRUSR
+)
 
 subprocess.Popen([
     'cmd',
