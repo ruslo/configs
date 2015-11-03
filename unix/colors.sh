149e3c30e9b0a90b279c7f5690af091cc00cedb6
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-11-03 14:06:49 +0600

    Make colors 'echo -e' friendly

--- unix/colors.sh
+++ unix/colors.sh
@@ -5,74 +5,74 @@
 # https://wiki.archlinux.org/index.php/Color_Bash_Prompt#List_of_colors_for_prompt_and_Bash
 
 # Reset
-export COLOR_OFF='\[\e[0m\]'       # Text Reset
+export COLOR_OFF='\e[0m'       # Text Reset
 
 # Regular Colors
-export COLOR_BLACK='\[\e[0;30m\]'        # Black
-export COLOR_RED='\[\e[0;31m\]'          # Red
-export COLOR_GREEN='\[\e[0;32m\]'        # Green
-export COLOR_YELLOW='\[\e[0;33m\]'       # Yellow
-export COLOR_BLUE='\[\e[0;34m\]'         # Blue
-export COLOR_PURPLE='\[\e[0;35m\]'       # Purple
-export COLOR_CYAN='\[\e[0;36m\]'         # Cyan
-export COLOR_WHITE='\[\e[0;37m\]'        # White
+export COLOR_BLACK='\e[0;30m'        # Black
+export COLOR_RED='\e[0;31m'          # Red
+export COLOR_GREEN='\e[0;32m'        # Green
+export COLOR_YELLOW='\e[0;33m'       # Yellow
+export COLOR_BLUE='\e[0;34m'         # Blue
+export COLOR_PURPLE='\e[0;35m'       # Purple
+export COLOR_CYAN='\e[0;36m'         # Cyan
+export COLOR_WHITE='\e[0;37m'        # White
 
 # Bold (B)
-export COLOR_B_BLACK='\[\e[1;30m\]'       # Black
-export COLOR_B_RED='\[\e[1;31m\]'         # Red
-export COLOR_B_GREEN='\[\e[1;32m\]'       # Green
-export COLOR_B_YELLOW='\[\e[1;33m\]'      # Yellow
-export COLOR_B_BLUE='\[\e[1;34m\]'        # Blue
-export COLOR_B_PURPLE='\[\e[1;35m\]'      # Purple
-export COLOR_B_CYAN='\[\e[1;36m\]'        # Cyan
-export COLOR_B_WHITE='\[\e[1;37m\]'       # White
+export COLOR_B_BLACK='\e[1;30m'       # Black
+export COLOR_B_RED='\e[1;31m'         # Red
+export COLOR_B_GREEN='\e[1;32m'       # Green
+export COLOR_B_YELLOW='\e[1;33m'      # Yellow
+export COLOR_B_BLUE='\e[1;34m'        # Blue
+export COLOR_B_PURPLE='\e[1;35m'      # Purple
+export COLOR_B_CYAN='\e[1;36m'        # Cyan
+export COLOR_B_WHITE='\e[1;37m'       # White
 
 # Underline (U)
-export COLOR_U_BLACK='\[\e[4;30m\]'       # Black
-export COLOR_U_RED='\[\e[4;31m\]'         # Red
-export COLOR_U_GREEN='\[\e[4;32m\]'       # Green
-export COLOR_U_YELLOW='\[\e[4;33m\]'      # Yellow
-export COLOR_U_BLUE='\[\e[4;34m\]'        # Blue
-export COLOR_U_PURPLE='\[\e[4;35m\]'      # Purple
-export COLOR_U_CYAN='\[\e[4;36m\]'        # Cyan
-export COLOR_U_WHITE='\[\e[4;37m\]'       # White
+export COLOR_U_BLACK='\e[4;30m'       # Black
+export COLOR_U_RED='\e[4;31m'         # Red
+export COLOR_U_GREEN='\e[4;32m'       # Green
+export COLOR_U_YELLOW='\e[4;33m'      # Yellow
+export COLOR_U_BLUE='\e[4;34m'        # Blue
+export COLOR_U_PURPLE='\e[4;35m'      # Purple
+export COLOR_U_CYAN='\e[4;36m'        # Cyan
+export COLOR_U_WHITE='\e[4;37m'       # White
 
 # Background (ON)
-export COLOR_ON_BLACK='\[\e[40m\]'       # Black
-export COLOR_ON_RED='\[\e[41m\]'         # Red
-export COLOR_ON_GREEN='\[\e[42m\]'       # Green
-export COLOR_ON_YELLOW='\[\e[43m\]'      # Yellow
-export COLOR_ON_BLUE='\[\e[44m\]'        # Blue
-export COLOR_ON_PURPLE='\[\e[45m\]'      # Purple
-export COLOR_ON_CYAN='\[\e[46m\]'        # Cyan
-export COLOR_ON_WHITE='\[\e[47m\]'       # White
+export COLOR_ON_BLACK='\e[40m'       # Black
+export COLOR_ON_RED='\e[41m'         # Red
+export COLOR_ON_GREEN='\e[42m'       # Green
+export COLOR_ON_YELLOW='\e[43m'      # Yellow
+export COLOR_ON_BLUE='\e[44m'        # Blue
+export COLOR_ON_PURPLE='\e[45m'      # Purple
+export COLOR_ON_CYAN='\e[46m'        # Cyan
+export COLOR_ON_WHITE='\e[47m'       # White
 
 # High Intensity (HI)
-export COLOR_HI_BLACK='\[\e[0;90m\]'       # Black
-export COLOR_HI_RED='\[\e[0;91m\]'         # Red
-export COLOR_HI_GREEN='\[\e[0;92m\]'       # Green
-export COLOR_HI_YELLOW='\[\e[0;93m\]'      # Yellow
-export COLOR_HI_BLUE='\[\e[0;94m\]'        # Blue
-export COLOR_HI_PURPLE='\[\e[0;95m\]'      # Purple
-export COLOR_HI_CYAN='\[\e[0;96m\]'        # Cyan
-export COLOR_HI_WHITE='\[\e[0;97m\]'       # White
+export COLOR_HI_BLACK='\e[0;90m'       # Black
+export COLOR_HI_RED='\e[0;91m'         # Red
+export COLOR_HI_GREEN='\e[0;92m'       # Green
+export COLOR_HI_YELLOW='\e[0;93m'      # Yellow
+export COLOR_HI_BLUE='\e[0;94m'        # Blue
+export COLOR_HI_PURPLE='\e[0;95m'      # Purple
+export COLOR_HI_CYAN='\e[0;96m'        # Cyan
+export COLOR_HI_WHITE='\e[0;97m'       # White
 
 # Bold High Intensity (BHI)
-export COLOR_BHI_BLACK='\[\e[1;90m\]'      # Black
-export COLOR_BHI_RED='\[\e[1;91m\]'        # Red
-export COLOR_BHI_GREEN='\[\e[1;92m\]'      # Green
-export COLOR_BHI_YELLOW='\[\e[1;93m\]'     # Yellow
-export COLOR_BHI_BLUE='\[\e[1;94m\]'       # Blue
-export COLOR_BHI_PURPLE='\[\e[1;95m\]'     # Purple
-export COLOR_BHI_CYAN='\[\e[1;96m\]'       # Cyan
-export COLOR_BHI_WHITE='\[\e[1;97m\]'      # White
+export COLOR_BHI_BLACK='\e[1;90m'      # Black
+export COLOR_BHI_RED='\e[1;91m'        # Red
+export COLOR_BHI_GREEN='\e[1;92m'      # Green
+export COLOR_BHI_YELLOW='\e[1;93m'     # Yellow
+export COLOR_BHI_BLUE='\e[1;94m'       # Blue
+export COLOR_BHI_PURPLE='\e[1;95m'     # Purple
+export COLOR_BHI_CYAN='\e[1;96m'       # Cyan
+export COLOR_BHI_WHITE='\e[1;97m'      # White
 
 # High Intensity backgrounds (ON_BHI)
-export COLOR_ON_BHI_IBLACK='\[\e[0;100m\]'   # Black
-export COLOR_ON_BHI_IRED='\[\e[0;101m\]'     # Red
-export COLOR_ON_BHI_IGREEN='\[\e[0;102m\]'   # Green
-export COLOR_ON_BHI_IYELLOW='\[\e[0;103m\]'  # Yellow
-export COLOR_ON_BHI_IBLUE='\[\e[0;104m\]'    # Blue
-export COLOR_ON_BHI_IPURPLE='\[\e[10;95m\]'  # Purple
-export COLOR_ON_BHI_ICYAN='\[\e[0;106m\]'    # Cyan
-export COLOR_ON_BHI_IWHITE='\[\e[0;107m\]'   # White
+export COLOR_ON_BHI_IBLACK='\e[0;100m'   # Black
+export COLOR_ON_BHI_IRED='\e[0;101m'     # Red
+export COLOR_ON_BHI_IGREEN='\e[0;102m'   # Green
+export COLOR_ON_BHI_IYELLOW='\e[0;103m'  # Yellow
+export COLOR_ON_BHI_IBLUE='\e[0;104m'    # Blue
+export COLOR_ON_BHI_IPURPLE='\e[10;95m'  # Purple
+export COLOR_ON_BHI_ICYAN='\e[0;106m'    # Cyan
+export COLOR_ON_BHI_IWHITE='\e[0;107m'   # White
