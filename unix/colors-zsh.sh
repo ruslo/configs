b6c14a270f6710659c332db5b69ab6f0add1b53b
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-11-29 15:34:36 +0600

    ZSH colors

---
+++ unix/colors-zsh.sh
@@ -0,0 +1,78 @@
+# Copyright (c) 2013, Ruslan Baratov
+# All rights reserved.
+
+# Bash colors
+# https://wiki.archlinux.org/index.php/Color_Bash_Prompt#List_of_colors_for_prompt_and_Bash
+
+# Reset
+export COLOR_OFF="`echo -e '\e[0m'`"       # Text Reset
+
+# Regular Colors
+export COLOR_BLACK="`echo -e '\e[0;30m'`"        # Black
+export COLOR_RED='\e[0;31m'          # Red
+export COLOR_GREEN="`echo -e '\e[0;32m'`"        # Green
+export COLOR_YELLOW="`echo -e '\e[0;33m'`"       # Yellow
+export COLOR_BLUE="`echo -e '\e[0;34m'`"         # Blue
+export COLOR_PURPLE="`echo -e '\e[0;35m'`"       # Purple
+export COLOR_CYAN="`echo -e '\e[0;36m'`"         # Cyan
+export COLOR_WHITE="`echo -e '\e[0;37m'`"        # White
+
+# Bold (B)
+export COLOR_B_BLACK="`echo -e '\e[1;30m'`"       # Black
+export COLOR_B_RED="`echo -e '\e[1;31m'`"         # Red
+export COLOR_B_GREEN="`echo -e '\e[1;32m'`"       # Green
+export COLOR_B_YELLOW="`echo -e '\e[1;33m'`"      # Yellow
+export COLOR_B_BLUE="`echo -e '\e[1;34m'`"        # Blue
+export COLOR_B_PURPLE="`echo -e '\e[1;35m'`"      # Purple
+export COLOR_B_CYAN="`echo -e '\e[1;36m'`"        # Cyan
+export COLOR_B_WHITE="`echo -e '\e[1;37m'`"       # White
+
+# Underline (U)
+export COLOR_U_BLACK="`echo -e '\e[4;30m'`"       # Black
+export COLOR_U_RED="`echo -e '\e[4;31m'`"         # Red
+export COLOR_U_GREEN="`echo -e '\e[4;32m'`"       # Green
+export COLOR_U_YELLOW="`echo -e '\e[4;33m'`"      # Yellow
+export COLOR_U_BLUE="`echo -e '\e[4;34m'`"        # Blue
+export COLOR_U_PURPLE="`echo -e '\e[4;35m'`"      # Purple
+export COLOR_U_CYAN="`echo -e '\e[4;36m'`"        # Cyan
+export COLOR_U_WHITE="`echo -e '\e[4;37m'`"       # White
+
+# Background (ON)
+export COLOR_ON_BLACK="`echo -e '\e[40m'`"       # Black
+export COLOR_ON_RED="`echo -e '\e[41m'`"         # Red
+export COLOR_ON_GREEN="`echo -e '\e[42m'`"       # Green
+export COLOR_ON_YELLOW="`echo -e '\e[43m'`"      # Yellow
+export COLOR_ON_BLUE="`echo -e '\e[44m'`"        # Blue
+export COLOR_ON_PURPLE="`echo -e '\e[45m'`"      # Purple
+export COLOR_ON_CYAN="`echo -e '\e[46m'`"        # Cyan
+export COLOR_ON_WHITE="`echo -e '\e[47m'`"       # White
+
+# High Intensity (HI)
+export COLOR_HI_BLACK="`echo -e '\e[0;90m'`"       # Black
+export COLOR_HI_RED="`echo -e '\e[0;91m'`"         # Red
+export COLOR_HI_GREEN="`echo -e '\e[0;92m'`"       # Green
+export COLOR_HI_YELLOW="`echo -e '\e[0;93m'`"      # Yellow
+export COLOR_HI_BLUE="`echo -e '\e[0;94m'`"        # Blue
+export COLOR_HI_PURPLE="`echo -e '\e[0;95m'`"      # Purple
+export COLOR_HI_CYAN="`echo -e '\e[0;96m'`"        # Cyan
+export COLOR_HI_WHITE="`echo -e '\e[0;97m'`"       # White
+
+# Bold High Intensity (BHI)
+export COLOR_BHI_BLACK="`echo -e '\e[1;90m'`"      # Black
+export COLOR_BHI_RED="`echo -e '\e[1;91m'`"        # Red
+export COLOR_BHI_GREEN="`echo -e '\e[1;92m'`"      # Green
+export COLOR_BHI_YELLOW="`echo -e '\e[1;93m'`"     # Yellow
+export COLOR_BHI_BLUE="`echo -e '\e[1;94m'`"       # Blue
+export COLOR_BHI_PURPLE="`echo -e '\e[1;95m'`"     # Purple
+export COLOR_BHI_CYAN="`echo -e '\e[1;96m'`"       # Cyan
+export COLOR_BHI_WHITE="`echo -e '\e[1;97m'`"      # White
+
+# High Intensity backgrounds (ON_BHI)
+export COLOR_ON_BHI_IBLACK="`echo -e '\e[0;100m'`"   # Black
+export COLOR_ON_BHI_IRED="`echo -e '\e[0;101m'`"     # Red
+export COLOR_ON_BHI_IGREEN="`echo -e '\e[0;102m'`"   # Green
+export COLOR_ON_BHI_IYELLOW="`echo -e '\e[0;103m'`"  # Yellow
+export COLOR_ON_BHI_IBLUE="`echo -e '\e[0;104m'`"    # Blue
+export COLOR_ON_BHI_IPURPLE="`echo -e '\e[10;95m'`"  # Purple
+export COLOR_ON_BHI_ICYAN="`echo -e '\e[0;106m'`"    # Cyan
+export COLOR_ON_BHI_IWHITE="`echo -e '\e[0;107m'`"   # White
